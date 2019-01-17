1a993737ac4f68ee946b1bebb1fe6752d10aaac7
Paul <35303983+paulfazilleau@users.noreply.github.com>
2019-01-17 13:51:24 +0100

    add index.2

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var numberUser = 0;
+var diceMax = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  numberUser++;
   console.log('a user connected');
+  console.log('Il y a ' + numberUser + 'user de connecte.');
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
@@ -16,6 +21,45 @@ io.on('connection', function(socket){
   });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+});
+
+
+function gameon() {
+  for (let index = 0; index < numberUser; index++) {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    if (dice > diceMax) {
+      diceMax = dice;
+      user = "le type";//nom de l'user
+    }
+  }
+  console.log("${user} as won");
+}
+window.setInterval(gameon, 10000);
+
+
+ // when the user disconnects.. perform this
+socket.on('disconnect', () => {
+  if (addedUser) {
+    --numUsers;
+
+    // echo globally that this client has left
+    socket.broadcast.emit('user left', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  }
+});
+
+socket.on('add user', (username) => {
+  if (addedUser) return;
+
+  // we store the username in the socket session for this client
+  socket.username = username;
+  ++numUsers;
+  addedUser = true;
+  socket.emit('login', {
+    numUsers: numUsers
   });
 });
 
