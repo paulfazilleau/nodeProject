8d860149813c8c8318d6c8edd1edc30061d150ef
Paul <35303983+paulfazilleau@users.noreply.github.com>
2019-01-17 16:47:14 +0100

    Change user count

--- index.js
+++ index.js
@@ -1,70 +1,58 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var numberUser = 0;
-var diceMax = 0;
+const bodyParser = require("body-parser");
 
 app.use(express.static('html'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+    console.log(req.body.user.name)
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/index.html');
 });
 
 io.on('connection', function(socket){
-  numberUser++;
-  console.log('a user connected');
-  console.log('Il y a ' + numberUser + 'user de connecte.');
+  numberUser = numberUser + 1;
+  console.log(socket.id + ' s\'est connecté');
+  console.log('Il y a ' + numberUser + ' utilisateur de connecté.');
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('disconnect', function() {
+    numberUser = numberUser - 1;
+    console.log(socket.id + ' s\'est déconnecté');
   });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-  socket.on('disconnect', () => {
-    if (addedUser) {
-      --numUsers;
 
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
-      });
-    }
-  });
-  socket.on('add user', (username) => {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-  });
+  socket.on('sendMessage', function (data) {
+    socket.broadcast.emit('message', data);
+    socket.emit('message', { text: data.text });   
+    }); 
 });
-
-
-function gameon() {
-  for (let index = 0; index < numberUser; index++) {
-    let dice = Math.floor(Math.random() * 6) + 1;
-    if (dice > diceMax) {
-      diceMax = dice;
-      user = "le type";//nom de l'user
-    }
-  }
-  console.log("${user} as won");
-}
-setInterval(gameon, 10000);
-
-
- // when the user disconnects.. perform this
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('Ecoute sur le port :3000');
 });
+
+
+
+
+//var numberUser = 0;
+//var diceMax = 0;
+// function gameon() {
+//   for (let index = 0; index < numberUser; index++) {
+//     let dice = Math.floor(Math.random() * 6) + 1;
+//     if (dice > diceMax) {
+//       diceMax = dice;
+//       user = "le type";//nom de l'user
+//     }
+//   }
+//   console.log("${user} as won");
+// }
+// window.setInterval(gameon, 10000);
